b2cabc569ba0e40e12bf445f2809edcf7c1c62b5
ice-bou <omar.bouattay@8-rays.dev>
2023-12-28 12:10:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PlayerList from './Components/PlayerList';
+
+function App() {
+  return (
+    <div>
+      <h1>Checkpoint PROPS</h1>
+      <PlayerList/>
+    </div>
+  );
+}
+
+export default App;
